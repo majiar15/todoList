cccab19e35196842f80dac8f95c775eb82963ef9
Martin Jimenez <majiar16@gmail.com>
2021-03-06 21:09:01 -0500

    deploy

--- src/helpers/procesoPesado.js
+++
@@ -1,7 +0,0 @@
-export const procesoPesado = (iteraciones) =>{
-    for (let i = 0; i < iteraciones; i++) {
-        console.log("alli vamos...")
-        
-    }
-    return `${iteraciones} iteraciones realizadas`;
-}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { HookApp } from "./hook.App";
 
-// import { CounterApp } from './components/01-useState/counterApp';
-// import { CounterWithCustumHook } from './components/01-useState/counterWithCustomHook';
-// import { SimpleForm } from "./components/01-useState/02-useEfect/SimpleForm";
-// import { FormWithCustomHook } from "./components/01-useState/02-useEfect/FormWithCustomHook";
-// import { MultipleCustomHooks } from './components/01-useState/03-examples/MultipleCustomHooks';
-// import { FocusScreen } from './components/01-useState/04-useRef/FocusScreen';
-// import { RealExampleRef } from './components/01-useState/04-useRef/RealExampleRef';
-// import { Layout } from './components/01-useState/05-useLayoutEffect/Layout';
-// import { Memorize } from './components/01-useState/06-memorize/Memorize';
-// import { MemoHook } from './components/01-useState/06-memorize/MemoHook';
-// import { CallbackHook } from './components/01-useState/06-memorize/CallbackHook';
-// import { Padre } from "./components/07-tarea-memo/Padre";
 import { TodoApp } from './components/08-useReducer/TodoApp';
 
 ReactDOM.render(
